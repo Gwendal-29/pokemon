d3b4dc93170cc2b6ec63169f3a5c3c75b6ab1d59
gwendal-29 <gwendal.vasseur29800@gmail.com>
2024-03-12 17:18:23 +0100

    add type

--- Type.js
+++ Type.js
@@ -1,11 +1,31 @@
 
 class Type
 {
-    static allType = {};
+    static all_types = {};
 
-    constructor()
+    constructor(name, effectiveness)
+    {
+        this._name = name;
+        this._effectiveness = effectiveness;
+    }
+
+    toString()
     {
 
     }
 
+    get name(){
+        return this._name;
+    }
+    get effectiveness(){
+        return this._effectiveness;
+    }
+
+    getEffectivenessByType(type)
+    {
+        return this._effectiveness[type] || 1.0;
+
+    }
+
 }
+
