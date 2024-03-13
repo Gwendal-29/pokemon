3a8dce0448ac57b9888d74ba36a8216a1459fdf3
KazouDev <frenchkazou@gmail.com>
2024-03-13 13:30:42 +0100

    Add attacks in poke

--- Pokemon.js
+++ Pokemon.js
@@ -59,6 +59,22 @@ class Pokemon {
         pokemon
         .filter((p) => p.form == "Normal")
         .forEach((p) => {
+            let types = pokemon_type.find((ptype) => ptype.form == "Normal" && ptype.pokemon_name == p.pokemon_name).type;
+
+            types.map((t) => {
+                if (!Type.all_types[t]){
+                    Type.all_types[t] = new Type(t);    
+                }
+            });
+
+            let attacks = pokemon_moves.find((pattack) => pattack.form == "Normal" && pattack.pokemon_name == p.pokemon_name);
+            [...attacks.charged_moves, ...attacks.fast_moves]
+                .map((a) => {
+                    if (!Attack.all_attacks[a]){
+                        Attack.all_attacks[a] = new Attack(a);    
+                    }
+                })
+
             Pokemon.all_pokemons[p.pokemon_id] = new Pokemon(p.pokemon_name, p.pokemon_id, p.form, p.base_stamina, p.base_defense, p.base_attack,
              this.get_gen(p.pokemon_name), 
              pokemon_type.find((ptype) => ptype.form == "Normal" && p.pokemon_name == ptype.pokemon_name).type);
@@ -71,6 +87,14 @@ class Pokemon {
                 return gen;
             }
         } 
-        return "Unknown génération."
+        return "Unknown generation."
+    }
+
+    getTypes(){
+        return this._types.map((t) => Type.all_types[t]);
+    }
+
+    getAttacks(){
+        return this._attack.map((a) => Attack.all_attacks[a]);
     }
 }
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 Pokemon.import_pokemon();
 
 console.log(Pokemon.all_pokemons);
+
+console.log(Pokemon.all_pokemons[3].getTypes());
