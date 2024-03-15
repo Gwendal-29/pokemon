2bc58a38603de00baca608ad40a03380bc7a2998
KazouDev <frenchkazou@gmail.com>
2024-03-15 13:36:03 +0100

    add func in type

--- Type.js
+++ Type.js
@@ -25,5 +25,35 @@ class Type
         return this._effectiveness[type] || 1.0;
 
     }
+
+    getTypeWithMinimumEffectiveness() {
+        const entries = Object.entries(this.effectiveness);
+    
+        const [minTypes, _] = entries.reduce((acc, [type, effectiveness]) => {
+            if (effectiveness < acc[1]) {
+                return [[type], effectiveness];
+            } else if (effectiveness === acc[1]) {
+                acc[0].push(type);
+            }
+            return acc;
+        }, [[], Infinity]);
+    
+        return minTypes;
+    }
+
+    getTypeWithMaximumEffectiveness() {
+        const entries = Object.entries(this.effectiveness);
+    
+        const [maxTypes, _] = entries.reduce((acc, [type, effectiveness]) => {
+            if (effectiveness > acc[1]) {
+                return [[type], effectiveness];
+            } else if (effectiveness === acc[1]) {
+                acc[0].push(type);
+            }
+            return acc;
+        }, [[], 0]);
+    
+        return maxTypes;
+    }
 }
 
