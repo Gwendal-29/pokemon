5ca4d23563d373d4020d4b311ee6eec4dd38f942
KazouDev <frenchkazou@gmail.com>
2024-03-15 15:29:17 +0100

    finish part 1

---
+++ data/class_type.js
@@ -0,0 +1,29 @@
+class Type
+{
+    static all_types = {};
+
+    constructor(name)
+    {
+        this._name = name;
+        this._effectiveness = type_effectiveness[name] || [];
+    }
+
+    toString()
+    {
+
+    }
+
+    get name(){
+        return this._name;
+    }
+    get effectiveness(){
+        return this._effectiveness;
+    }
+
+    getEffectivenessByType(type)
+    {
+        return this._effectiveness[type] || 1.0;
+
+    }
+}
+
